0667ecbb04958d1796a0229acd3ad548561ba70d
fransasdsa <calizayaapazaf@gmail.com>
2024-10-05 12:00:58 -0500

    first commitijhflkgh

---
+++ ms-auth-service/src/test/java/com/tuempresa/msauthservice/MsAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuempresa.msauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
